85652b89adb44e3456d91bfc047edd10f9d1e6bc
Ankit Sharma <ankitsharma11876@gmail.com>
2024-11-22 17:36:56 +0530

    using axios method here

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,32 @@
-// import { useState } from "react";
-// function App() {
-//   const [count, setCount] = useState(0);
+import axios from "axios"
+import { useState } from "react"
 
-//   // Event handler function
-//   const handleClick = () => {
-//     setCount(count + 1);
-//   };
+const App = () => {
+  
+    const [data, setData] = useState([])
 
-//   const handleClick2 = () => {
-//     setCount(count - 1);
-//   };
-
-
-//   return (
-//     <div>
-//       <p>You clicked {count} times</p>
-//       {/* Button with onClick event handler */}
-//       <button onClick={handleClick}>
-
-//         Click me
-//       </button>
-
-//       <button onClick={handleClick2}>
-//         Click Me
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-import  { useState } from 'react';
-
-function App() {
-  const [message, setMessage] = useState("");
-
-  const handleMessage = (text) => {
-    setMessage(text);
-  };
+   const getData = async ()=>{
+    
+    const response =  await axios.get('https://picsum.photos/v2/list?page=2&limit=10')
+    
+       setData(response.data)
+       
+       
+   }
 
   return (
-    <div>
-      <p>{message}</p>
-      {/* Pass arguments to the event handler */}
-      <button onClick={() => handleMessage('Hello from the button!')}>
-        Send Message
-      </button>
+    <div className="p-10">
+       <button onClick={getData} className="bg-teal-700  text-white font-semibold text-2xl px-6 py-3  rounded active:scale-90">Get Data</button>
+       <div className="p-5 mt-5 bg-gray-950">
+            {data.map(function(elem,idx){
+               return <div key={idx} className="bg-gray-50 text-black flex items-center justify-between w-full px-7 py-6 rounded mb-3">
+                      <img className="h-40" src={elem.download_url} alt=""/> 
+                      <h1>{elem.author}</h1>
+                </div>
+            })}
+       </div>
     </div>
-  );
+  )
 }
 
-export default App;
-
+export default App
---
+++ src/Form.jsx
@@ -0,0 +1,46 @@
+// import { useState } from "react"
+
+// const Form = () => {
+   
+//    const [username, setUsername] = useState(" ");
+//   const submitHandler = (e) =>{
+//      e.preventDefault()
+//       console.log(username);
+//       setUsername("")
+      
+//   }
+
+
+//   return (
+//     <div>
+//       <form onSubmit={(e)=>{
+//           submitHandler(e)
+//       }}>
+//           <input value={username} className="px-4  rounded py-3 text-xl m-5 text-black " type='text' onChange={(e)=>{
+//              setUsername(e.target.value)
+//           }}  placeholder="Enter Your Name"/>
+//           <button className="px-4 py-3 m-5 text-xl font-semibold bg-emerald-600 rounded text-white">Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default Form
+
+
+// @ here i learn 2 way binding
+
+
+
+
+
+import React from 'react'
+
+
+const Form = () => {
+  return (
+    <div>Form</div>
+  )
+}
+
+export default Form
